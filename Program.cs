898010c28db1edb1735743d72b3661a35e8b6092
GodwynMiles <godwynmiles12@gmail.com>
2023-08-29 15:40:37 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FirstApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace MyApp
+{
+
+}
+
---
+++ Program2.cs
@@ -0,0 +1,46 @@
+﻿using MyApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication
+{
+
+    class MyProgram
+    {
+       //Creating an Array
+        static void Main(string[] args)
+        {
+            string[] dogNames = { "Molly", "Winter", "Gucci" };
+            int[] dogAges = { 3, 2, 1 };
+
+            Console.WriteLine("Enter Dog Name: ");
+            string NameOfDogs = Console.ReadLine();
+
+            bool contains = false;
+            int AgeOfDogs = 0;
+
+            for (int i = 0; i < dogNames.Length; i++)
+            {
+                if (NameOfDogs == dogNames[i])
+                {
+                    contains = true;
+                    AgeOfDogs = dogAges[i];
+                    break;
+                }
+            }
+
+            if (contains == true)
+            {
+                Console.WriteLine("{0} is now {1} year/s old", NameOfDogs, AgeOfDogs);
+            }
+            else
+            {
+                Console.WriteLine("default");
+            }            
+        }
+    }
+}
